cb42a236e6b47e7c9abe43304b2bbcd0386301ce
Vasiliy Smolyar <vasiliy.dpk@gmail.com>
2016-12-27 21:47:41 +0200

    Added clean less project

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
                     optimization: 2
                 },
                 files: {
-                    "css/main.css": "development/less/main.less"
+                    "css/main.css": "development/less/_main.less"
                 }
             }
         },
@@ -42,7 +42,7 @@ module.exports = function(grunt) {
                 separator: ';',
             },
             dist: {
-                src: ['development/libs/jquery/jquery-3.1.1.js', 'development/libs/**/*.js', 'development/js/custom.js'],
+                src: ['development/libs/jquery/jquery-3.1.1.js', 'development/libs/**/*.js', 'development/js/_custom.js'],
                 dest: 'development/js/concat_scripts.js',
             },
         },
